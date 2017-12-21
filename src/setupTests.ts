e8c98afbe695de433a2b248ff89daa8bdcc75a67
Chang-Steven <steven.chang.dev@gmail.com>
2017-12-21 13:31:35 -0800

    Added component unit testing

---
+++ src/setupTests.ts
@@ -0,0 +1,9 @@
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+//
+// Enzyme.configure({ adapter: new Adapter() });
+
+
+import * as enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
